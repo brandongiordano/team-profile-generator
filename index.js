2e9f1ab2ae624ba6bb874d1f060c8c09babec556
brandongiordano <bgiordano9917@aol.com>
2023-01-24 17:19:03 -0500

    added prompt to make new manager

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const inquirer = require("inquirer");
+constfs = require("fs");
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const teamMembers = [];
+
+const promptManager = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Please enter your name",
+        },
+        {
+            type: "input",
+            name: "employeeId",
+            message: "Please enter your employee ID",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Please enter your office number",
+        }
+    ]).then(answers => {
+        const manager = new Manager(
+            answers.name, answers.employeeId, answers.email, answers.officeNumber,
+        )
+        teamMembers.push(manager);
+    })
+}
